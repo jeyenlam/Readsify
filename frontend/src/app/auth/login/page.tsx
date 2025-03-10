eeaaa052e9d3b6f974a7df3895a1b3b8f9452618
jeyenlam <yenlam.roxal@gmail.com>
2025-03-09 22:49:33 -0400

    styled login

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import React, { useState } from 'react'
 import { useAppContext } from '@/app/provider'
+import logo from '../../../../public/R.png'
+import Image from 'next/image'
 
 const LogIn = () => {
   const { handleLogIn } = useAppContext()
@@ -10,25 +12,41 @@ const LogIn = () => {
   })
 
   return (
-    <div className='flex justify-center'>
+    <div className='flex flex-col justify-center items-center h-screen bg-gradient-to-br from-teal-200 to-blue-300'>
+      <Image src={logo} alt='logo' width={100} />
       <form 
         onSubmit={(e) => {
           e.preventDefault(); 
           handleLogIn(logInFormData)
           }}
-        className='bg-slate-200'
+        className='mt-5 bg-slate-100 flex flex-col gap-5 p-5 rounded-md border border-sinc-200'
       >
-        <div className='grid grid-cols-2'>
+        <p className='text-center text-sm'>Reasify, an AI-powered book recommender web app</p>
+        <hr/>
+        <div className='grid grid-cols-2 gap-5'>
           <label>Username: </label>
-          <input type='text'name='name'value={logInFormData.username} required
+          <input 
+            type='text'
+            name='name'
+            value={logInFormData.username}
+            required
             onChange={(e) => setLogInFormData({...logInFormData, username: e.target.value})}
           />
           <label>Password: </label>
-          <input type='password' name='password' value={logInFormData.password} required
+          <input
+            type='password'
+            name='password'
+            value={logInFormData.password}
+            required
             onChange={(e) => setLogInFormData({...logInFormData, password: e.target.value})}
           />
         </div>
-        <button className='default-button' type='submit'>Log in</button>
+        <button className='w-full bg-teal-600 rounded-md text-slate-50 hover:bg-teal-500 p-1' type='submit'>Log in</button>
+        <hr/>
+        <div className='flex gap-2 col-span-2 items-center justify-center'>
+          <p>Don't have an account?</p>
+          <a href='/auth/signup' className='hover:text-teal-600'>Sign up</a>
+        </div>
       </form>
     </div>
   )
